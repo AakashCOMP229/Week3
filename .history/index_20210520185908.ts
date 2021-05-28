68266dbf6173cc7d557b598ab42349d081b2298b
AakashCOMP229 <a563@my.centennialcollege.ca>
2021-05-28 15:55:41 +0530

    upgraded project to use  express

---
+++ .history/index_20210520185908.ts
@@ -0,0 +1,17 @@
+const http = require('http'); // import built-in http module
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//instantiate new http server and store a reference to it in "server"
+const server = http.createServer((req, res) => 
+{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello in my World');
+});
+
+server.listen(port, hostname, () => 
+{
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
